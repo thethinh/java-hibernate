8f2fca10a699418328a72e83ae5e1b2d0ed8b6fc
ThinhNguyen <39476468+thethinh@users.noreply.github.com>
2020-04-09 20:48:16 +0700

    Add files via upload

---
+++ Nguyễn Thế Thịnh/OneToMany/src/main/java/hibernate/dao/DemoInsert.java
@@ -0,0 +1,37 @@
+package hibernate.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import hibernate.entities.Company;
+import hibernate.entities.Employee;
+
+public class DemoInsert {
+  public static void main(String[] args) {
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    entityManager.getTransaction().begin();
+    Company company = new Company();
+    company.setName("Google");
+    entityManager.persist(company);
+    
+    Employee emp1 = new Employee();
+    emp1.setName("kai");
+    emp1.setCompany(company);
+    
+    Employee emp2 = new Employee();
+    emp2.setName("sena");
+    emp2.setCompany(company);
+    
+    entityManager.persist(emp1);
+    entityManager.persist(emp2);
+    
+    entityManager.getTransaction().commit();
+    System.out.println("--------- after insert -------------");
+    System.out.println(company);
+    System.out.println(emp1);
+    System.out.println(emp2);
+    entityManager.close();
+    entityManagerFactory.close();
+  }
+}
---
+++ Nguyễn Thế Thịnh/OneToMany/src/main/java/hibernate/dao/DemoSelect.java
@@ -0,0 +1,22 @@
+package hibernate.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import hibernate.entities.Company;
+
+public class DemoSelect {
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Company company = entityManager.find(Company.class, 1);
+		System.out.println(company);
+
+		company.getListEmployee().forEach(employee -> System.out.println(employee));
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+}
